1bd60b7dfb7be316356be90aeb0580db1962719d
Yohei Okubo <okubo.yohei@gmail.com>
2018-08-25 18:52:29 +0900

    stub coding

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,26 @@
 import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button
+} from 'react-native';
 
 export default class App extends React.Component<{}> {
   render() {
+    const onPressButton = () => { alert('🤭🤭🤭') }
     return (
       <View style={styles.container}>
-        <Text>Open up src/App.js to start working on your app!</Text>
+        <Text>This is Prisma, GraphQL, ReactNative template App!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
+
+        <Button
+          onPress={onPressButton}
+          title="Nothing will happen"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+        />
       </View>
     );
   }
